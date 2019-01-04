cfe910bb24229e6ea572295fb434006bc47321ea
Mike Brucker <disposition11@gmail.com>
2019-01-04 15:36:32 -0500

    refactored server functions

--- functions/index.js
+++ functions/index.js
@@ -4,8 +4,8 @@ admin.initializeApp(functions.config().firebase);
 
 const updateTeam = (team) => {
 	return admin.firestore().collection('teams').doc(team.teamId).set(team)
-	.then(doc => {
-		console.log('updateTeam', team, doc);
+	.then(snapshot => {
+		console.log('updateTeam', team, snapshot);
 	})
 }
 
@@ -13,17 +13,11 @@ exports.teamCreated = functions.firestore
 	.document('teams/{teamId}')
 	.onCreate(doc => {
 		return admin.firestore().collection('teams')
-		.doc(doc.id).get().then(doc => {
-			const teamdata = doc.data();
+		.doc(doc.id).get().then(snapshot => {
+			const teamdata = snapshot.data();
 			const team = {
-				arena: teamdata.arena,
-				league: teamdata.league,
-				players: teamdata.players,
-				schedules: teamdata.schedules,
-				sport: teamdata.sport,
-				teamName: teamdata.teamName,
-				teamOwnerId: teamdata.teamOwnerId,
-				teamId: doc.id
+				...teamdata,
+				teamId: snapshot.id
 			}
 			return updateTeam(team);
 		})
